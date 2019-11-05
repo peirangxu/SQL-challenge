a6b1074a20c53f11e653d5323d8648a03e8c7732
Peirang Xu <tennyxu1995@gmail.com>
2019-11-04 20:41:15 -0800

    sql change

--- EmployeeSQL/table schemata.sql
+++ EmployeeSQL/table schemata.sql
@@ -1,126 +1,103 @@
 --------------------------------------------------------------------------------------------------------------
---List the following details of each employee: employee number, last name, first name, gender, and salary
-DROP TABLE IF EXISTS employee_detail;
+--Import departments.csv
+DROP TABLE IF EXISTS departments;
 
-CREATE TABLE employee_detail AS
-SELECT employees.emp_no,
-	employees.last_name,
-	employees.first_name,
-	employees.gender,
-	salaries.salary
-FROM employees
-JOIN salaries ON
-employees.emp_no = salaries.emp_no;
+CREATE TABLE departments (
+	dept_no VARCHAR Not Null Primary Key,
+	dept_name VARCHAR Not Null);
 
-COPY employee_detail TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_detail.csv' 
+COPY departments
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/departments.csv'
 DELIMITER ',' CSV HEADER;
+
+SELECT * FROM departments;
+
 --------------------------------------------------------------------------------------------------------------
---List employees who were hired in 1986
-DROP TABLE IF EXISTS employee_hired_1986;
+-- Import employees.csv
+DROP TABLE IF EXISTS employees;
 
-CREATE TABLE employee_hired_1986 AS
-SELECT last_name,first_name,hire_date
-FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+CREATE TABLE employees (
+	emp_no INT Not Null Primary Key,
+	birth_date DATE Not Null,
+	first_name VARCHAR Not Null,
+	last_name VARCHAR Not Null,
+	gender VARCHAR Not Null,
+	hire_date DATE Not Null);
 
-COPY employee_hired_1986 TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_hired_1986.csv' 
+COPY employees
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/employees.csv'
 DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------------
--- List the manager of each department with the following information: 
---department number, department name, the manager's employee number, last name, first name, and start and end employment dates	
-DROP TABLE IF EXISTS dept_manager_info;
 
-CREATE TABLE dept_manager_info AS
-SELECT departments.dept_no,
-	departments.dept_name,
-	dept_manager.emp_no,
-	employees.last_name,
-	employees.first_name,
-	dept_manager.from_date,
-	dept_manager.to_date
-FROM departments
-JOIN dept_manager ON 
-departments.dept_no = dept_manager.dept_no
-JOIN employees ON
-dept_manager.emp_no = employees.emp_no;
+SELECT * FROM employees;
 
-COPY dept_manager_info TO '/Users/peirangxu/Desktop/SQL-challenge/Output/dept_manager_info.csv' 
+--------------------------------------------------------------------------------------------------------------
+-- Import dept_emp.csv
+DROP TABLE IF EXISTS dept_emp;
+
+CREATE TABLE dept_emp (
+	emp_no INT Not Null,
+	dept_no VARCHAR Not Null,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	from_date DATE,
+	to_date DATE,
+	Primary Key (emp_no,dept_no));
+
+COPY dept_emp
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/dept_emp.csv'
 DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
---List the department of each employee with the following information: employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_dept;
 
-CREATE TABLE employee_dept AS
-SELECT employees.emp_no,
-	employees.last_name,
-	employees.first_name,
-	departments.dept_name
-FROM departments
-JOIN dept_emp ON 
-	departments.dept_no = dept_emp.dept_no
-JOIN employees ON
-	dept_emp.emp_no = employees.emp_no;
+SELECT * FROM dept_emp;
 
-COPY employee_dept TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_dept.csv' 
+--------------------------------------------------------------------------------------------------------------
+-- Import dept_manager.csv
+DROP TABLE IF EXISTS dept_manager;
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR Not Null,
+	emp_no INT Not Null,
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	from_date DATE Not Null,
+	to_date DATE Not Null,
+	Primary Key (dept_no,emp_no));
+
+COPY dept_manager
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/dept_manager.csv'
 DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
---List all employees whose first name is "Hercules" and last names begin with "B."
-DROP TABLE IF EXISTS Hercules_B_;
 
-CREATE TABLE Hercules_B_ AS
-SELECT last_name, first_name
-FROM employees
-WHERE first_name = 'Hercules' AND last_name like 'B%';
+SELECT * FROM dept_manager;
 
-COPY Hercules_B_ TO '/Users/peirangxu/Desktop/SQL-challenge/Output/Hercules_B_.csv' 
+--------------------------------------------------------------------------------------------------------------
+--Import salaries.csv
+DROP TABLE IF EXISTS salaries;
+
+CREATE TABLE salaries (
+	emp_no INT Not Null Primary Key,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	salary INT Not Null,
+	from_date DATE Not Null,
+	to_date DATE Not Null);
+
+COPY salaries
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/salaries.csv'
 DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
---List all employees in the Sales department, including their employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_sales;
 
-CREATE TABLE employee_sales AS
-SELECT employees.emp_no,
-	employees.last_name,
-	employees.first_name,
-	departments.dept_name
-FROM departments
-JOIN dept_emp ON 
-	departments.dept_no = dept_emp.dept_no
-JOIN employees ON
-	dept_emp.emp_no = employees.emp_no
-WHERE departments.dept_name = 'Sales';
+SELECT * FROM salaries;
 
-COPY employee_sales TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales.csv' 
+--------------------------------------------------------------------------------------------------------------
+-- Import titles.csv
+DROP TABLE IF EXISTS titles;
+
+CREATE TABLE titles (
+	emp_no INT Not Null,
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	title VARCHAR Not Null,
+	from_date DATE Not Null,
+	to_date DATE Not Null);
+
+COPY titles
+FROM '/Users/peirangxu/Desktop/USC-LA-DATA-PT-08-2019-U-C/09-SQL/Homework/data/titles.csv'
 DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
---List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_sales_development;
 
-CREATE TABLE employee_sales_development AS
-SELECT employees.emp_no,
-	employees.last_name,
-	employees.first_name,
-	departments.dept_name
-FROM departments
-JOIN dept_emp ON 
-	departments.dept_no = dept_emp.dept_no
-JOIN employees ON
-	dept_emp.emp_no = employees.emp_no
-WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Developments';
-
-COPY employee_sales_development TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales_development.csv' 
-DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
---In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
-DROP TABLE IF EXISTS last_name_freq;
-
-CREATE TABLE last_name_freq AS
-SELECT last_name,
-	COUNT(last_name)
-FROM employees
-GROUP BY last_name
-ORDER BY count DESC;
-
-COPY last_name_freq TO '/Users/peirangxu/Desktop/SQL-challenge/Output/last_name_freq.csv' 
-DELIMITER ',' CSV HEADER;
------------------------------------------------------------------------------------------------------------------------------
+SELECT * FROM titles;
