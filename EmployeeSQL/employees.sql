a85978e912995ff19a8ea67c28a075627f519c45
Peirang Xu <tennyxu1995@gmail.com>
2019-11-04 18:25:28 -0800

    output csv

--- EmployeeSQL/employees.sql
+++ EmployeeSQL/employees.sql
@@ -1,3 +1,4 @@
+--------------------------------------------------------------------------------------------------------------
 --Import departments.csv
 DROP TABLE IF EXISTS departments;
 
@@ -11,6 +12,7 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM departments;
 
+--------------------------------------------------------------------------------------------------------------
 -- Import employees.csv
 DROP TABLE IF EXISTS employees;
 
@@ -28,7 +30,7 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM employees;
 
-
+--------------------------------------------------------------------------------------------------------------
 -- Import dept_emp.csv
 DROP TABLE IF EXISTS dept_emp;
 
@@ -47,6 +49,7 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM dept_emp;
 
+--------------------------------------------------------------------------------------------------------------
 -- Import dept_manager.csv
 DROP TABLE IF EXISTS dept_manager;
 
@@ -65,6 +68,7 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM dept_manager;
 
+--------------------------------------------------------------------------------------------------------------
 --Import salaries.csv
 DROP TABLE IF EXISTS salaries;
 
@@ -81,6 +85,7 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM salaries;
 
+--------------------------------------------------------------------------------------------------------------
 -- Import titles.csv
 DROP TABLE IF EXISTS titles;
 
@@ -97,7 +102,11 @@ DELIMITER ',' CSV HEADER;
 
 SELECT * FROM titles;
 
+--------------------------------------------------------------------------------------------------------------
 --List the following details of each employee: employee number, last name, first name, gender, and salary
+DROP TABLE IF EXISTS employee_detail;
+
+CREATE TABLE employee_detail AS
 SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
@@ -107,13 +116,25 @@ FROM employees
 JOIN salaries ON
 employees.emp_no = salaries.emp_no;
 
+COPY employee_detail TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_detail.csv' 
+DELIMITER ',' CSV HEADER;
+--------------------------------------------------------------------------------------------------------------
 --List employees who were hired in 1986
+DROP TABLE IF EXISTS employee_hired_1986;
+
+CREATE TABLE employee_hired_1986 AS
 SELECT last_name,first_name,hire_date
 FROM employees
 WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
 
+COPY employee_hired_1986 TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_hired_1986.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------------
 -- List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates	
+DROP TABLE IF EXISTS dept_manager_info;
+
+CREATE TABLE dept_manager_info AS
 SELECT departments.dept_no,
 	departments.dept_name,
 	dept_manager.emp_no,
@@ -127,7 +148,13 @@ departments.dept_no = dept_manager.dept_no
 JOIN employees ON
 dept_manager.emp_no = employees.emp_no;
 
+COPY dept_manager_info TO '/Users/peirangxu/Desktop/SQL-challenge/Output/dept_manager_info.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------
 --List the department of each employee with the following information: employee number, last name, first name, and department name
+DROP TABLE IF EXISTS employee_dept;
+
+CREATE TABLE employee_dept AS
 SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
@@ -138,12 +165,19 @@ JOIN dept_emp ON
 JOIN employees ON
 	dept_emp.emp_no = employees.emp_no;
 
+COPY employee_dept TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_dept.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------
 --List all employees whose first name is "Hercules" and last names begin with "B."
 SELECT last_name, first_name
 FROM employees
 WHERE first_name = 'Hercules' AND last_name like 'B%';
 
+-----------------------------------------------------------------------------------------------------------------------------
 --List all employees in the Sales department, including their employee number, last name, first name, and department name
+DROP TABLE IF EXISTS employee_sales;
+
+CREATE TABLE employee_sales AS
 SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
@@ -155,7 +189,13 @@ JOIN employees ON
 	dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales';
 
+COPY employee_sales TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
+DROP TABLE IF EXISTS employee_sales_development;
+
+CREATE TABLE employee_sales_development AS
 SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
@@ -167,10 +207,19 @@ JOIN employees ON
 	dept_emp.emp_no = employees.emp_no
 WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Developments';
 
+COPY employee_sales_development TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales_development.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
+DROP TABLE IF EXISTS last_name_freq;
+
+CREATE TABLE last_name_freq AS
 SELECT last_name,
 	COUNT(last_name)
 FROM employees
 GROUP BY last_name
 ORDER BY count DESC;
 
+COPY last_name_freq TO '/Users/peirangxu/Desktop/SQL-challenge/Output/last_name_freq.csv' 
+DELIMITER ',' CSV HEADER;
+-----------------------------------------------------------------------------------------------------------------------------
