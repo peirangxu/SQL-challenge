c5c7017d2cfe75f6df03dce37f39fa10762656eb
Peirang Xu <tennyxu1995@gmail.com>
2019-11-07 13:00:01 -0800

    code update

--- EmployeeSQL/EmployeeSQL.sql
+++ EmployeeSQL/EmployeeSQL.sql
@@ -104,38 +104,30 @@ SELECT * FROM titles;
 
 --------------------------------------------------------------------------------------------------------------
 --List the following details of each employee: employee number, last name, first name, gender, and salary
-DROP TABLE IF EXISTS employee_detail;
-
-CREATE TABLE employee_detail AS
-SELECT employees.emp_no,
+COPY
+(SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
 	employees.gender,
 	salaries.salary
 FROM employees
 JOIN salaries ON
-employees.emp_no = salaries.emp_no;
-
-COPY employee_detail TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_detail.csv' 
+employees.emp_no = salaries.emp_no)
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/employee_detail.csv' 
 DELIMITER ',' CSV HEADER;
 --------------------------------------------------------------------------------------------------------------
 --List employees who were hired in 1986
-DROP TABLE IF EXISTS employee_hired_1986;
-
-CREATE TABLE employee_hired_1986 AS
-SELECT last_name,first_name,hire_date
+COPY
+(SELECT last_name,first_name,hire_date
 FROM employees
-WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
-
-COPY employee_hired_1986 TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_hired_1986.csv' 
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31')
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/employee_hired_1986.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------------
 -- List the manager of each department with the following information: 
 --department number, department name, the manager's employee number, last name, first name, and start and end employment dates	
-DROP TABLE IF EXISTS dept_manager_info;
-
-CREATE TABLE dept_manager_info AS
-SELECT departments.dept_no,
+COPY
+(SELECT departments.dept_no,
 	departments.dept_name,
 	dept_manager.emp_no,
 	employees.last_name,
@@ -146,16 +138,13 @@ FROM departments
 JOIN dept_manager ON 
 departments.dept_no = dept_manager.dept_no
 JOIN employees ON
-dept_manager.emp_no = employees.emp_no;
-
-COPY dept_manager_info TO '/Users/peirangxu/Desktop/SQL-challenge/Output/dept_manager_info.csv' 
+dept_manager.emp_no = employees.emp_no)
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/dept_manager_info.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
 --List the department of each employee with the following information: employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_dept;
-
-CREATE TABLE employee_dept AS
-SELECT employees.emp_no,
+COPY
+(SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
 	departments.dept_name
@@ -163,22 +152,21 @@ FROM departments
 JOIN dept_emp ON 
 	departments.dept_no = dept_emp.dept_no
 JOIN employees ON
-	dept_emp.emp_no = employees.emp_no;
-
-COPY employee_dept TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_dept.csv' 
+	dept_emp.emp_no = employees.emp_no)
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/employee_dept.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
 --List all employees whose first name is "Hercules" and last names begin with "B."
+COPY(
 SELECT last_name, first_name
 FROM employees
-WHERE first_name = 'Hercules' AND last_name like 'B%';
-
+WHERE first_name = 'Hercules' AND last_name like 'B%')
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/Hercules_B_.csv' 
+DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
 --List all employees in the Sales department, including their employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_sales;
-
-CREATE TABLE employee_sales AS
-SELECT employees.emp_no,
+COPY
+(SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
 	departments.dept_name
@@ -187,16 +175,13 @@ JOIN dept_emp ON
 	departments.dept_no = dept_emp.dept_no
 JOIN employees ON
 	dept_emp.emp_no = employees.emp_no
-WHERE departments.dept_name = 'Sales';
-
-COPY employee_sales TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales.csv' 
+WHERE departments.dept_name = 'Sales')
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/employee_sales.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
 --List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name
-DROP TABLE IF EXISTS employee_sales_development;
-
-CREATE TABLE employee_sales_development AS
-SELECT employees.emp_no,
+COPY
+(SELECT employees.emp_no,
 	employees.last_name,
 	employees.first_name,
 	departments.dept_name
@@ -205,21 +190,17 @@ JOIN dept_emp ON
 	departments.dept_no = dept_emp.dept_no
 JOIN employees ON
 	dept_emp.emp_no = employees.emp_no
-WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Developments';
-
-COPY employee_sales_development TO '/Users/peirangxu/Desktop/SQL-challenge/Output/employee_sales_development.csv' 
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Developments')
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/employee_sales_development.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
 --In descending order, list the frequency count of employee last names, i.e., how many employees share each last name
-DROP TABLE IF EXISTS last_name_freq;
-
-CREATE TABLE last_name_freq AS
-SELECT last_name,
+COPY
+(SELECT last_name,
 	COUNT(last_name)
 FROM employees
 GROUP BY last_name
-ORDER BY count DESC;
-
-COPY last_name_freq TO '/Users/peirangxu/Desktop/SQL-challenge/Output/last_name_freq.csv' 
+ORDER BY count DESC)
+TO '/Users/peirangxu/Desktop/SQL-challenge/Output_CSV/last_name_freq.csv' 
 DELIMITER ',' CSV HEADER;
 -----------------------------------------------------------------------------------------------------------------------------
